32df3f04c92bc4d5a3c663ac96b242842eb4276e
Veljo Lasn <vlasn@users.noreply.github.com>
2024-12-03 09:58:16 +0200

    patch: viewport resize jump issue

--- extensions/cornerstone/src/services/ViewportService/CornerstoneViewportService.ts
+++ extensions/cornerstone/src/services/ViewportService/CornerstoneViewportService.ts
@@ -1043,6 +1043,13 @@ class CornerstoneViewportService extends PubSubService implements IViewportServi
       // Store the current position presentations for each viewport.
       viewports.forEach(({ id: viewportId }) => {
         const presentation = this._getPositionPresentation(viewportId);
+        // @BMPATCH
+        // https://github.com/OHIF/Viewers/blob/543aa137dc691f557ee5098f035c99f3cbc687ed/extensions/cornerstone/src/services/ViewportService/CornerstoneViewportService.ts
+        // During a resize, the slice index should remain unchanged. This is a temporary fix for
+        // a larger issue regarding the definition of slice index with slab thickness.
+        // We need to revisit this to make it more robust and understandable.
+        delete presentation.viewReference.sliceIndex;
+        // /@BMPATCH
         this.beforeResizePositionPresentations.set(viewportId, presentation);
       });
 
